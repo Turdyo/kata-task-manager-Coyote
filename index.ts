d9e96a4d542d862129b64c8bd17d1984b4188a28
Turdyo <martin.gayet@efrei.net>
2023-11-15 11:18:37 +0100

    feat(task manager): add todo

--- index.ts
+++ index.ts
@@ -2,12 +2,29 @@ import { Database } from "bun:sqlite"
 
 const db = new Database("db.sqlite")
 
+type Todo = {
+    id: number
+    description: string
+    status: "done" | "to do"
+}
+
 export function parse(input: string): { operation: string, argument?: string } {
     const operation = input.at(0) as string
     if (input.length === 1) return { operation }
-
     const argument = input.slice(2)
-    console.log(argument)
-
     return { operation, argument }
+}
+
+export class TaskManager {
+    todos: Todo[] = []
+    id: number = 1
+
+    addTodo(description: string) {
+        this.todos.push({
+            description,
+            id: this.id,
+            status: "to do"
+        })
+        this.id = this.id + 1
+    }
 }
--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,5 +1,5 @@
-import { describe, test, expect } from "bun:test";
-import { parse } from "..";
+import { describe, test, expect, beforeEach } from "bun:test";
+import { TaskManager, parse } from "..";
 
 describe("parse", () => {
     test("case '+'", () => {
@@ -8,5 +8,40 @@ describe("parse", () => {
             operation: "+",
             argument: "New todo"
         })
+    })
+    test("case 'q'", () => {
+        const result = parse("q")
+        expect(result).toEqual({
+            operation: "q"
+        })
+    })
+})
+
+describe("add operation", () => {
+    let MyTaskManager: TaskManager
+    beforeEach(() => {
+        MyTaskManager = new TaskManager()
+    })
+    test("normal case", () => {
+        MyTaskManager.addTodo("Learn Java")
+        expect(MyTaskManager.todos).toEqual([{
+            id: 1,
+            description: "Learn Java",
+            status: "to do"
+        }])
+    })
+    test("multiple todos", () => {
+        const todos = ["Learn Python", "Learn C#"]
+        todos.forEach(todo => MyTaskManager.addTodo(todo))
+        expect(MyTaskManager.todos).toEqual([{
+            id: 1,
+            description: "Learn Python",
+            status: "to do"
+        },
+        {
+            id: 2,
+            description: "Learn C#",
+            status: "to do"
+        }])
     })
 })
